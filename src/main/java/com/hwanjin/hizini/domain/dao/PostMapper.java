fac35580938a1b2df6041c6b1892664c61bc5296
joohwan <joohon812@nate.com>
2021-12-25 23:04:47 +0900

    서비스 추가

--- src/main/java/com/hwanjin/hizini/domain/dao/PostMapper.java
+++ src/main/java/com/hwanjin/hizini/domain/dao/PostMapper.java
@@ -2,10 +2,18 @@ package com.hwanjin.hizini.domain.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.lang.Nullable;
+
+import com.hwanjin.hizini.domain.dto.ImageDTO;
 import com.hwanjin.hizini.domain.dto.PostDTO;
 
 public interface PostMapper {
-
-	public List<PostDTO> selectPost();
+	
+	public int createPost(PostDTO post);
+	public int createImage(@Param("postId") int postId, ImageDTO image);
+	public List<PostDTO> selectPost(@Nullable PostDTO post);
+	public int updatePost(PostDTO post);
+	public int deletePost(String id);
 
 }
